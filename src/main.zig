ffb30668ac3d7a285e5973562dd6380846c32c27
QuentinTessier <tessier.quentin.pro@gmail.com>
2025-07-08 08:29:53 +0200

    single radix sort is working

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,7 @@ const Inlucere = @import("Inlucere");
 const Mesh = @import("./3D/Mesh.zig");
 const math = @import("zmath");
 const SinglePoolAllocator = @import("./graphics/SinglePoolAllocator.zig").GPUSinglePoolAllocator;
+const ObjectPoolAllocator = @import("./graphics/ObectPoolAllocator.zig").ObjectPoolAllocator;
 const MeshManager = @import("./3D/MeshManager.zig");
 const ecez = @import("ecez");
 const Components = @import("components.zig");
@@ -108,11 +109,35 @@ pub const Engine = struct {
     pub fn deinit(self: *Engine, allocator: std.mem.Allocator) void {
         self.storage.deinit();
         allocator.destroy(self.storage);
-        self.mesh_manager.deinit();
-        allocator.destroy(self.mesh_manager);
         self.device.deinit();
         allocator.destroy(self.device);
         self.window.destroy();
+    }
+};
+
+pub const PushConstant = extern struct {
+    g_num_elements: u32,
+};
+
+pub const SortContext = struct {
+    executed: bool = false,
+    data: []const u32,
+    push_constant: Inlucere.Device.DynamicBuffer,
+    buffer1: Inlucere.Device.DynamicBuffer,
+    buffer2: Inlucere.Device.DynamicBuffer,
+
+    pub fn init(data: []const u32) !SortContext {
+        std.log.info("Creating a sorting context for {} elements", .{data.len});
+        const size_as_u32: PushConstant = .{ .g_num_elements = @intCast(data.len) };
+        const push_constant: Inlucere.Device.DynamicBuffer = try .init("radix_sort_push_constants", std.mem.asBytes(&size_as_u32), @sizeOf(PushConstant));
+        const buffer1: Inlucere.Device.DynamicBuffer = try .init("radix_sort_buffer_1", std.mem.sliceAsBytes(data), @sizeOf(u32));
+        const buffer2: Inlucere.Device.DynamicBuffer = try .initEmpty("radix_sort_buffer_2", @intCast(data.len * @sizeOf(u32)), @sizeOf(u32));
+        return .{
+            .data = data,
+            .push_constant = push_constant,
+            .buffer1 = buffer1,
+            .buffer2 = buffer2,
+        };
     }
 };
 
@@ -127,6 +152,13 @@ pub fn main() !void {
         _ = debug_allocator.deinit();
     };
 
+    var prng = std.Random.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    const rand = prng.random();
+
     var engine: Engine = undefined;
     defer engine.deinit(allocator);
 
@@ -136,37 +168,43 @@ pub fn main() !void {
     glfw.windowHint(.context_version_major, 4);
     glfw.windowHint(.context_version_minor, 6);
     glfw.windowHint(.opengl_profile, .opengl_core_profile);
+    glfw.windowHint(.opengl_debug_context, true);
     engine.window = try glfw.Window.create(1280, 720, "lucens", null);
     glfw.makeContextCurrent(engine.window);
 
-    try Inlucere.init(glfw.getProcAddress);
+    var ext = try std.BoundedArray([]const u8, 32).init(0);
+    try ext.append("GL_KHR_shader_subgroup_arithmetic");
+    try Inlucere.init(glfw.getProcAddress, ext.slice());
     engine.device = try allocator.create(Inlucere.Device);
     try engine.device.init(allocator);
 
     engine.storage = try allocator.create(Storage);
     engine.storage.* = try .init(allocator);
 
-    engine.mesh_manager = try allocator.create(MeshManager);
+    _ = try engine.device.createComputePipeline("SingleRadixSortPipeline", @embedFile("single_radixsort.comp"));
 
-    engine.mesh_manager.* = try .init(allocator);
+    const data = try allocator.alloc(u32, 16);
+    defer allocator.free(data);
+    for (data, 0..) |*item, i| {
+        item.* = @intCast(i);
+    }
+    rand.shuffle(u32, data);
 
-    const suzanne = try engine.mesh_manager.loadObj("./assets/meshes/suzanne.obj");
-    _ = try engine.mesh_manager.makeGPUResident(suzanne);
+    for (data) |item| {
+        std.debug.print("{} ", .{item});
+    }
+    std.debug.print("\n", .{});
 
-    const sphere = try engine.mesh_manager.loadObj("./assets/meshes/sphere.obj");
-    _ = try engine.mesh_manager.makeGPUResident(sphere);
+    var sort_context: SortContext = try .init(data);
 
-    const e = try engine.storage.createEntity(.{ Components.Material{
-        .base_color = .{ 1, 0, 0, 1 },
-    }, Components.MeshID{
-        .id = suzanne,
-    }, Components.Transform{
-        .cached_matrix = undefined,
-        .position = .{ 0, 0, 0 },
-        .scale = .{ 1, 1, 1 },
-        .rotation = .{ 0, 0, 0 },
-    } });
-    _ = e;
+    // const gpu_mapped = Inlucere.gl.mapNamedBuffer(sort_context.buffer2.handle, Inlucere.gl.READ_ONLY);
+    // if (gpu_mapped == null) {
+    //     std.log.err("Failed to map memory", .{});
+    //     return;
+    // }
+    // const gpu_memory: [*]u32 = @ptrCast(@alignCast(gpu_mapped.?));
+    // const gpu_memory_slice = gpu_memory[0..data.len];
+    // @memcpy(data, gpu_memory_slice);
 
     var last_time = glfw.getTime();
     while (!engine.window.shouldClose()) {
@@ -179,6 +217,39 @@ pub fn main() !void {
         engine.device.clearSwapchain(.{
             .colorLoadOp = .clear,
         });
+
+        if (!sort_context.executed) {
+            sort_context.executed = true;
+
+            if (engine.device.bindComputePipeline("SingleRadixSortPipeline")) {
+                engine.device.bindUniformBuffer(2, sort_context.push_constant.toBuffer(), .whole());
+                engine.device.bindStorageBuffer(0, sort_context.buffer1.toBuffer(), .whole());
+                engine.device.bindStorageBuffer(1, sort_context.buffer2.toBuffer(), .whole());
+                engine.device.dispatch(1, 1, 1);
+                engine.device.setMemoryBarrier(.{
+                    .ShaderStorageBarrier = true,
+                });
+            }
+
+            var sync = engine.device.fence();
+            const res = try sync.wait(Inlucere.gl.TIMEOUT_IGNORED);
+            std.log.info("Done sorting !", .{});
+
+            switch (res) {
+                .Success => {
+                    Inlucere.gl.getNamedBufferSubData(sort_context.buffer2.handle, 0, @intCast(data.len * @sizeOf(u32)), @ptrCast(data.ptr));
+                },
+                .Timeout => {
+                    std.log.warn("Fence timeout", .{});
+                },
+            }
+            sync.deinit();
+
+            for (data) |item| {
+                std.debug.print("{} ", .{item});
+            }
+            std.debug.print("\n", .{});
+        }
 
         engine.window.swapBuffers();
     }
